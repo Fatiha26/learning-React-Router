20f04795c0da8592e08d70bd1162bb72f996478a
Fatiha26 <f6lamia@gmail.com>
2024-08-13 22:18:17 +0600

    useNavigate hook

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom"
 
 const About = () => {
+
+  const navigate =   useNavigate();
+
+  const goto = () =>{
+    navigate("/contact");
+  }
+
   return (
     <>
       <div>This is the About Page</div>
+      <button onClick={()=>goto()}>Go to contact page</button>
+      <button onClick={()=>{navigate(-1)}}>GO back</button>
     </>
   )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,17 @@
+import { useNavigate } from "react-router-dom"
+
 const Contact = () => {
+  const navigate = useNavigate();
+
+  const goto = () => {
+    navigate("/")
+  }
   return (
     <>
         <div>This is the Contact Page</div>
+        <button onClick={()=>goto()}>Go to Home page</button>
+        <button onClick={()=>{navigate(-1)}}>GO back</button>
+
     </>
   )
 }
