7153a92d68e711a219a6f25af2149b6c2f6cdd28
Fatiha26 <f6lamia@gmail.com>
2024-08-13 20:44:06 +0600

    browser router, routes,route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>React Router</div>
+    <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<div>hello</div>} />
+            <Route path="/about" element={<div>About Page!</div>}/>
+            <Route path="/contact" element={<div>Contact</div>}/>
+          </Routes>          
+        </BrowserRouter>
+    </>
   )
 }
 
